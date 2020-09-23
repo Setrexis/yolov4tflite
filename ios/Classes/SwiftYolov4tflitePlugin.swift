39f079f9d86855b773583bfbdada5e0ce0b688d6
Setrexis <seban.nadler@web.de>
2020-09-23 17:28:14 +0200

    Update SwiftYolov4tflitePlugin.swift

--- ios/Classes/SwiftYolov4tflitePlugin.swift
+++ ios/Classes/SwiftYolov4tflitePlugin.swift
@@ -43,19 +43,18 @@ public class SwiftYolov4tflitePlugin: NSObject, FlutterPlugin {
 
         detectObjects(imagePath: path,result: result)
     }
-    
   }
 
   func loadModel(labelData: String,modelFileKey: String,result: @escaping FlutterResult){
         DispatchQueue.global(qos: .userInitiated).async {
-            try{
-                detector = Yolov4Classifier(labelData: labelData,modelFileKey: modelFileKey)
+            do{
+                detector = try Yolov4Classifier(labelData: labelData,modelFileKey: modelFileKey)
                 DispatchQueue.main.sync{
                     result("Succsess")
                 }
-            }catch{
+            }catch let error{
                 DispatchQueue.main.sync{
-                    result("Error")
+                    result(error.localizedDescription)
                 }
             }
         }
@@ -63,21 +62,52 @@ public class SwiftYolov4tflitePlugin: NSObject, FlutterPlugin {
 
     func detectObjects(imagePath:String, result: @escaping FlutterResult){
         DispatchQueue.global(qos: .userInitiated).async {
-            try{
+            do{
+                let imageData = try Data(contentsOf: fileURL)
+                let image = UIImage(data: imageData)
+                let pixelBuffer = buffer(image: image)
                 
-                
-                let recognitions = detector.runModel(pixelBuffer: )
+                let recognitions = try detector.runModel(pixelBuffer: pixelBuffer)
 
-
-                DispatchQueue.main.sync{
-                    result("Succsess")
+                var reg : [String] = []
+                for r in recognitions{
+                    reg.append(String(r))
                 }
-            }catch{
+
                 DispatchQueue.main.sync{
-                    result("Error")
+                    result(reg)
+                }
+            }catch let error{
+                DispatchQueue.main.sync{
+                    result(error.localizedDescription)
                 }
             }
         }
+    }
+
+    func buffer(from image: UIImage) -> CVPixelBuffer? {
+        let attrs = [kCVPixelBufferCGImageCompatibilityKey: kCFBooleanTrue, kCVPixelBufferCGBitmapContextCompatibilityKey: kCFBooleanTrue] as CFDictionary
+        var pixelBuffer : CVPixelBuffer?
+        let status = CVPixelBufferCreate(kCFAllocatorDefault, Int(image.size.width), Int(image.size.height), kCVPixelFormatType_32ARGB, attrs, &pixelBuffer)
+        guard (status == kCVReturnSuccess) else {
+            return nil
+        }
+
+        CVPixelBufferLockBaseAddress(pixelBuffer!, CVPixelBufferLockFlags(rawValue: 0))
+        let pixelData = CVPixelBufferGetBaseAddress(pixelBuffer!)
+
+        let rgbColorSpace = CGColorSpaceCreateDeviceRGB()
+        let context = CGContext(data: pixelData, width: Int(image.size.width), height: Int(image.size.height), bitsPerComponent: 8, bytesPerRow: CVPixelBufferGetBytesPerRow(pixelBuffer!), space: rgbColorSpace, bitmapInfo: CGImageAlphaInfo.noneSkipFirst.rawValue)
+
+        context?.translateBy(x: 0, y: image.size.height)
+        context?.scaleBy(x: 1.0, y: -1.0)
+
+        UIGraphicsPushContext(context!)
+        image.draw(in: CGRect(x: 0, y: 0, width: image.size.width, height: image.size.height))
+        UIGraphicsPopContext()
+        CVPixelBufferUnlockBaseAddress(pixelBuffer!, CVPixelBufferLockFlags(rawValue: 0))
+
+        return pixelBuffer
     }
 }
 
